f85e2723485c61f1219ffe4ce9969720793666a4
booldook <booldook@gmail.com>
2021-03-22 13:48:32 +0900

    ...

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
-$(".main-wrapper .pager-wrapper a.pager").click(function(){
+$(".main-wrapper .pager-wrapper a.pager").click(function(e){
+	e.preventDefault();
 	$(".main-wrapper .pager-wrapper a.pager").removeClass('active')
 	$(this).addClass('active')
 })
 
-$(".main-wrapper .pager-wrapper .bt-play").click(function(){
+$(".main-wrapper .pager-wrapper .bt-play").click(function(e){
+	e.preventDefault();
 	$(".main-wrapper .pager-wrapper .bt-play").addClass('active')
 	$(".main-wrapper .pager-wrapper .bt-pause").removeClass('active')
 })
 
-$(".main-wrapper .pager-wrapper .bt-pause").click(function(){
+$(".main-wrapper .pager-wrapper .bt-pause").click(function(e){
+	e.preventDefault();
 	$(".main-wrapper .pager-wrapper .bt-play").removeClass('active')
 	$(".main-wrapper .pager-wrapper .bt-pause").addClass('active')
+})
+
+$(".best-wrapper .pager-wrapper a.pager").click(function(e){
+	e.preventDefault();
+	$(".best-wrapper .pager-wrapper a.pager").removeClass('active')
+	$(this).addClass('active')
 })
